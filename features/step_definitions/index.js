7fa50c1cba5ee6a4c99dcbe88127deed15012ac7
mavenlink <jon@mavenlink.com>
2015-01-25 12:03:41 -0800

    add example

---
+++ features/step_definitions/index.js
@@ -0,0 +1,21 @@
+// foo bar baz
+
+var myApplicationsSpecialSteps = function() {
+  this.Given(/^I am on Mavenlink$/, function(callback) {
+    //this.google.visitGoogle(callback);
+    console.log("using special ml callback");
+    callback();
+  });
+
+  this.When(/^I search for "(.*)"$/, function(query, callback) {
+    //this.google.query(query, callback);
+    callback();
+  });
+
+  this.Then(/^I see a link to "(.*)"$/, function(url, callback) {
+    //this.google.assertDisplayedLinkToURL(url, callback);
+    callback();
+  });
+};
+
+module.exports = myApplicationsSpecialSteps;
